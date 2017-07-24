d2ec12685a95b7f48661ae95d8008b32ce7a90d2
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 15:54:53 -0700

    Factorial test shouldThrowIllegalArgumentExceptionWhenNumberIsNegative passes.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException {
         if (i==1 || i==0)   return 1;
         else if (i > 1)  return i*compute(i-1);
-        return -1;
+        throw new IllegalArgumentException("Cannot compute factorial of negative number.");
     }
 }
