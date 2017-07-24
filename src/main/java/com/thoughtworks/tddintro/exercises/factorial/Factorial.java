35cbf5bfabef5b020090a8221fb2ea88c3455407
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 15:48:46 -0700

    Refactored Factorial method compute.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -3,8 +3,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 public class Factorial {
     public Integer compute(int i) {
         if (i==1 || i==0)   return 1;
-        else if (i==2)   return 2;
-        else if (i > 2)  return i*compute(i-1);
+        else if (i > 1)  return i*compute(i-1);
         return -1;
     }
 }
