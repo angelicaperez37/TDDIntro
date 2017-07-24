14bd336c20dd1393bab1b9b46ff02762516b41c4
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 15:42:22 -0700

    Factorial test shouldReturnTwoWhenNumberIsTwo passes.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -3,6 +3,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 public class Factorial {
     public Integer compute(int i) {
         if (i==1)   return 1;
+        if (i==2)   return 2;
         return -1;
     }
 }
