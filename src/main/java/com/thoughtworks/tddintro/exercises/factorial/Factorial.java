d0f810c5565f1b49e989a98cbd1763cbc48a68c7
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 15:46:22 -0700

    Factorial test shouldReturnSixWhenNumberIsThree passes.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -3,7 +3,8 @@ package com.thoughtworks.tddintro.exercises.factorial;
 public class Factorial {
     public Integer compute(int i) {
         if (i==1 || i==0)   return 1;
-        if (i==2)   return 2;
+        else if (i==2)   return 2;
+        else if (i > 2)  return i*compute(i-1);
         return -1;
     }
 }
