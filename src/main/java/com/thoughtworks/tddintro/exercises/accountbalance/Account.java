4188700c1c2f2ed75749f9cb9c37e8ae74129963
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 16:07:49 -0700

    Account test shouldIncreaseMyBalanceWhenIDepositMoney passes.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account() {
+        balance = 0;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void makeDeposit(int amount) {
+        balance += amount;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,21 +3,25 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        account.makeDeposit(10);
+        assertTrue(account.getBalance()==10);
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    @Ignore
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    @Ignore
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
     }
