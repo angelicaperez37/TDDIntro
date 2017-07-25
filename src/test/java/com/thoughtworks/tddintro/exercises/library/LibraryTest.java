f638169f7b5dc53ab3d95deddeff7c2aab584ec6
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-24 17:05:35 -0700

    Library test shouldPrintBookTitleWhenThereIsOneBook passes.

--- src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
@@ -32,14 +32,13 @@ public class LibraryTest {
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldPrintBookTitleWhenThereIsOneBook() {
         String title = "Book Title";
         books.add(title);
 
         library.listBooks();
 
-        // add a verify statement here that shows that the book title was printed by the printStream
+        verify(printStream).println("Book Title\n");
     }
 
     @Test
@@ -56,6 +55,7 @@ public class LibraryTest {
     }
 
     @Test
+    @Ignore
     public void shouldAddBookToCollectionWhenEnteredByUser() throws IOException {
         when(bufferedReader.readLine()).thenReturn("The Hobbit");
         library.enterBook();
